c550ee823366df11b6f5c6f079e9583415a2e059
last-blade <meprashanttyagi2000@gmail.com>
2025-05-22 11:30:00 +0530

    Improve database connection log message to include database name

--- src/database/database.js
+++ src/database/database.js
@@ -4,7 +4,7 @@ import { DB_NAME } from "../constants.js"
 const connectDB = async () => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
-        console.log(`Database connected !!! Host ${connectionInstance.connection.host}`)
+        console.log(`Database connected !!! Host: ${connectionInstance.connection.host} / ${connectionInstance.connection.name}`);
     } catch (error) {
         console.log(`Error while connecting to database ${error.message}`);
         process.exit(1);
