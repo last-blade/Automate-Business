56b6d3f4bf9507153738e6b6e9f93a6230dec0ee
last-blade <meprashanttyagi2000@gmail.com>
2025-05-22 18:22:36 +0530

    Refactor token options in constants.js to separate access and refresh token configurations

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,13 @@
 export const DB_NAME = "automate-business-prashant-srpk"
-export const options = {
+export const accessTokenOptions = {
     httpOnly: true,
     secure: true,
     sameSite: "None",
     maxAge: 7 * 24 * 60 * 60 *1000,
+}
+export const refreshTokenOptions = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 28 * 24 * 60 * 60 *1000,
 }
