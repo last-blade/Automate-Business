597192073156372b442ecf0e0fdf9923958df133
last-blade <meprashanttyagi2000@gmail.com>
2025-05-22 11:10:21 +0530

    Initialize Express app with CORS, body parsing, and cookie parsing middleware

---
+++ src/app.js
@@ -0,0 +1,24 @@
+import express, { urlencoded } from "express"
+import dotenv from "dotenv"
+import cookieParser from "cookie-parser";
+import cors from "cors"
+
+dotenv.config({
+    path: "./.env"
+});
+
+const app = express();
+
+const corsOptions = {
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}
+
+app.use(cors(corsOptions));
+app.use(express.json({limit: "20kb"}));
+app.use(urlencoded({extended: true, limit: "20kb"}));
+app.use(cookieParser());
+app.use(express.static("public"));
+
+
+export { app }
