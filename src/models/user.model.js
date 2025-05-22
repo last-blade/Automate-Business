e3bfffe239e1c9c31e57f982a6ae9f5f4d11970d
last-blade <meprashanttyagi2000@gmail.com>
2025-05-22 12:20:54 +0530

    Add user authentication methods for token generation and password validation

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,6 @@
 import mongoose, {Schema} from "mongoose";
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
 
 const userSchema = new Schema({
     fullname: {
@@ -20,8 +22,35 @@ const userSchema = new Schema({
     }
 }, {timestamps: true});
 
+userSchema.methods.generateAccessToken = async function(){
+    const accessToken = await jwt.sign(
+        {
+            fullname: this.fullname,
+            email: this.email,
+            _id: this._id
+        },
 
+        process.env.ACCESS_TOKEN_SECRET,
 
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
 
+    return accessToken;
+};
+
+userSchema.methods.isPasswordCorrect = async function(password){
+    return await bcrypt.compare(password, this.password);
+}
+
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        return next();
+    }
+
+    this.password = await bcrypt.hash(this.password, 10);
+    return next();
+});
 
 export const User = mongoose.model("User", userSchema);
