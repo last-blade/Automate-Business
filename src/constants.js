f583bdefff095fb439302f96fdd3f26257e91a87
last-blade <meprashanttyagi2000@gmail.com>
2025-05-22 13:45:54 +0530

    Add options constant for cookie configuration

--- src/constants.js
+++ src/constants.js
@@ -1,1 +1,7 @@
 export const DB_NAME = "automate-business-prashant-srpk"
+export const options = {
+    httpOnly: true,
+    secure: true,
+    sameSite: "None",
+    maxAge: 7 * 24 * 60 * 60 *1000,
+}
