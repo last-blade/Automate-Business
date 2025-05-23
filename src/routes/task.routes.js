7f16240c18eed2fe7990649ef1fe008a474ea360
last-blade <meprashanttyagi2000@gmail.com>
2025-05-23 16:35:35 +0530

    Add fetch-all-tasks route for retrieving all tasks with authentication

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { createTask } from "../controllers/taskControllers/createTask.controller.js";
 import { viewTask } from "../controllers/taskControllers/viewTask.controller.js";
+import { fetchUsersAllTasks } from "../controllers/taskControllers/fetchUsersAllTasks.controller.js";
 
 const router = Router();
 
 router.route("/create-task").post(authMiddleware, createTask);
 
 router.route("/view-task/:taskId").get(authMiddleware, viewTask);
+router.route("/fetch-all-tasks").get(authMiddleware, fetchUsersAllTasks);
 
 export default router;
