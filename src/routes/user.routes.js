8c7a4b6732e66b6b3f1f025418e9ccef61d08303
last-blade <meprashanttyagi2000@gmail.com>
2025-05-23 14:02:14 +0530

    Add route for refreshAccessToken controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,6 +4,7 @@ import { loginUser } from "../controllers/userControllers/loginUser.controller.j
 import { changePassword } from "../controllers/userControllers/changePassword.controller.js";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { logoutUser } from "../controllers/userControllers/logoutUser.controller.js";
+import { refreshAccessToken } from "../controllers/userControllers/refreshAccessToken.controller.js";
 
 const router = Router();
 
@@ -11,5 +12,7 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/change-password").post(authMiddleware, changePassword);
 router.route("/logout").post(authMiddleware, logoutUser);
+
+router.route("/refresh-access-token").post(refreshAccessToken);
 
 export default router;
