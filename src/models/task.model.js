1f65dc35b3ff6a4c5abd1b24c128c998909db3d9
last-blade <meprashanttyagi2000@gmail.com>
2025-05-23 15:38:42 +0530

    Add Task model with schema definition for task management

---
+++ src/models/task.model.js
@@ -0,0 +1,87 @@
+import mongoose, { Schema } from "mongoose";
+
+const taskSchema = new Schema({
+    taskTitle: {
+        type: String,
+        required: true,
+    },
+
+    taskDescription: {
+        type: String,
+        required: true,
+    },
+
+    taskAssignee: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+
+    taskCategory: {
+        type: String,
+        required: true,
+    },
+
+    taskDueDate: {
+        type: Date,
+        // required: true,
+        default: Date.now,
+    },
+
+    taskPriority: {
+        type: String,
+        enum: ["High", "Medium", "Low"],
+        default: "Low",
+        // required: true,
+    },
+
+    taskFrequency: {
+        type: {
+            type: String,
+            enum: ["daily", "weekly", "monthly", "yearly", "periodically"],
+        },
+        details: {
+            daysOfWeek: [
+                {
+                    day: {
+                        type: String,
+                        enum: ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"],
+                        required: true,
+                    },
+                    date: {
+                        type: Date,
+                        required: true,
+                    }
+                }
+            ],
+
+            datesOfMonth: [
+                {
+                    type: Number,
+                    min: 1,
+                    max: 31
+                }
+            ],
+
+            yearlyDate: Date,
+
+            periodicDates: [Date]
+        }
+    },
+
+    taskStatus: {
+        type: String,
+        enum: ["Overdue", "Pending", "In Progress", "Completed"],
+        default: "Pending",
+    },
+
+    taskCreatedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    },
+
+
+}, {timestamps: true});
+
+export const Task = mongoose.model("Task", taskSchema);
