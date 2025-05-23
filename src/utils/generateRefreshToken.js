50a1c0acc762a3da46b16ec859349d6069d3d43e
last-blade <meprashanttyagi2000@gmail.com>
2025-05-23 10:19:39 +0530

    Refactor logging in generateRefreshToken function for debugging error

--- src/utils/generateRefreshToken.js
+++ src/utils/generateRefreshToken.js
@@ -2,9 +2,9 @@ import { User } from "../models/user.model.js"
 
 const generateRefreshToken = async (userId) => {
     const foundUser = await User.findById(userId);
-
+console.log(foundUser)
     const refreshToken = await foundUser.generateRefreshToken();
-    
+    console.log("refreshtoken",refreshToken)
     return refreshToken;
 }
 
