c0f8eeef9084ef38201642044f76406e57e11daf
last-blade <meprashanttyagi2000@gmail.com>
2025-05-23 10:32:05 +0530

    Add route for changePassword controller

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/userControllers/registerUser.controller.js";
 import { loginUser } from "../controllers/userControllers/loginUser.controller.js";
+import { changePassword } from "../controllers/userControllers/changePassword.controller.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+router.route("/change-password").post(changePassword);
 
 export default router;
