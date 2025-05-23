bb8c3e0aab038114369e19aba47d126224af7436
last-blade <meprashanttyagi2000@gmail.com>
2025-05-23 11:18:38 +0530

    Add authentication middleware to changePassword and logout routes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -2,11 +2,14 @@ import { Router } from "express";
 import { registerUser } from "../controllers/userControllers/registerUser.controller.js";
 import { loginUser } from "../controllers/userControllers/loginUser.controller.js";
 import { changePassword } from "../controllers/userControllers/changePassword.controller.js";
+import { authMiddleware } from "../middlewares/auth.middleware.js";
+import { logoutUser } from "../controllers/userControllers/logoutUser.controller.js";
 
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
-router.route("/change-password").post(changePassword);
+router.route("/change-password").post(authMiddleware, changePassword);
+router.route("/logout").post(authMiddleware, logoutUser);
 
 export default router;
