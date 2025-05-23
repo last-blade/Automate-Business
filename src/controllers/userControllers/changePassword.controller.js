2c3ed99dd0ce28b48e2ac2fd051c1f633816acab
last-blade <meprashanttyagi2000@gmail.com>
2025-05-23 10:31:57 +0530

    Add changePassword controller for user password updates

---
+++ src/controllers/userControllers/changePassword.controller.js
@@ -0,0 +1,27 @@
+import { apiError, apiResponse, asyncHandler, User } from "../allImports.js";
+
+const changePassword = asyncHandler(async (request, response) => {
+    const {email, incomingPassword} = request.body;
+
+    if([email, incomingPassword].some((inputField) => inputField.trim === "")){
+        throw new apiError(404, "All fields are required")
+    }
+
+    const foundUser = await User.findOne({email: email});
+
+    if(!foundUser){
+        throw new apiError(404, "User with this email does not exists")
+    }
+
+    foundUser.password = incomingPassword;
+    
+    foundUser.save({validateBeforeSave: false});
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, {}, "Password changed successfully")
+    )
+
+});
+
+export {changePassword}
