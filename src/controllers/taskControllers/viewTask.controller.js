203ea1b94e50cd7a6879c656d9096a5f0d2b2731
last-blade <meprashanttyagi2000@gmail.com>
2025-05-23 16:21:41 +0530

    Add viewTask controller for retrieving task details by ID

---
+++ src/controllers/taskControllers/viewTask.controller.js
@@ -0,0 +1,22 @@
+import { apiError, apiResponse, asyncHandler, Task } from "../allImports.js";
+
+const viewTask = asyncHandler(async (request, response) => {
+    const {taskId} = request?.params;
+
+    if(!taskId){
+        throw new apiError(404, "Task id not found!")
+    }
+
+    const foundTask = await Task.findById(taskId).select("-__v -_id");
+
+    if(!foundTask){
+        return response.status(404, {}, "Task not found, task may be deleted")
+    }
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, foundTask, "Task found successfully")
+    )
+});
+
+export {viewTask}
