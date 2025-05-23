77b205599ed9f86261a6a815cf6634680c2f49fe
last-blade <meprashanttyagi2000@gmail.com>
2025-05-23 17:06:54 +0530

    Add delete-task route for handling task deletion with authentication

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -3,6 +3,7 @@ import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { createTask } from "../controllers/taskControllers/createTask.controller.js";
 import { viewTask } from "../controllers/taskControllers/viewTask.controller.js";
 import { fetchUsersAllTasks } from "../controllers/taskControllers/fetchUsersAllTasks.controller.js";
+import { deleteTask } from "../controllers/taskControllers/deleteTask.controller.js";
 
 const router = Router();
 
@@ -10,5 +11,7 @@ router.route("/create-task").post(authMiddleware, createTask);
 
 router.route("/view-task/:taskId").get(authMiddleware, viewTask);
 router.route("/fetch-all-tasks").get(authMiddleware, fetchUsersAllTasks);
+
+router.route("/delete-task/:taskId").delete(authMiddleware, deleteTask);
 
 export default router;
