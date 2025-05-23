2c819e0da72a2c9e693156e309f110c00357e82e
last-blade <meprashanttyagi2000@gmail.com>
2025-05-23 17:06:46 +0530

    Add deleteTask controller for handling task deletion with error handling

---
+++ src/controllers/taskControllers/deleteTask.controller.js
@@ -0,0 +1,24 @@
+import { apiResponse, asyncHandler, Task } from "../allImports.js";
+
+const deleteTask = asyncHandler(async (request, response) => {
+    const {taskId} = request?.params;
+
+    if(!taskId){
+        throw new apiError(404, "Task id not found!")
+    }
+
+    const foundTask = await Task.findById(taskId);
+
+    if(!foundTask){
+        return response.status(404, {}, "Task not found, task may be deleted")
+    }
+
+    await Task.findByIdAndDelete(taskId);
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, {}, "Task deleted successfully")
+    )
+});
+
+export {deleteTask}
