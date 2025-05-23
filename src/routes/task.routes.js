32c4a7916276a629c24ca7b03c0872f1605c83ff
last-blade <meprashanttyagi2000@gmail.com>
2025-05-23 16:21:48 +0530

    Add view-task route for retrieving task details with authentication

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { createTask } from "../controllers/taskControllers/createTask.controller.js";
+import { viewTask } from "../controllers/taskControllers/viewTask.controller.js";
 
 const router = Router();
 
 router.route("/create-task").post(authMiddleware, createTask);
+
+router.route("/view-task/:taskId").get(authMiddleware, viewTask);
 
 export default router;
