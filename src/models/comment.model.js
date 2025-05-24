86d11151adf9a08696947de9f60ec297d437f147
last-blade <meprashanttyagi2000@gmail.com>
2025-05-24 14:40:22 +0530

    Add comment model with schema for storing comments related to tasks

---
+++ src/models/comment.model.js
@@ -0,0 +1,20 @@
+import mongoose, { Schema } from "mongoose";
+
+const commentSchema = new Schema({
+    comment: {
+        type: String,
+    },
+
+    commentedTask: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Task"
+    },
+
+    commentedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+    },
+
+}, {timestamps: true});
+
+export const Comment = mongoose.model("Comment", commentSchema);
