8ff7441ccca06e8fa4ad2ecc65323b6420e40817
last-blade <meprashanttyagi2000@gmail.com>
2025-05-24 11:23:08 +0530

    Add changeTaskStatus controller for updating task status with error handling

---
+++ src/controllers/taskControllers/changeTaskStatus.controller.js
@@ -0,0 +1,38 @@
+import { apiError, apiResponse, asyncHandler, Task } from "../allImports.js";
+
+const changeTaskStatus = asyncHandler(async (request, response) => {
+    const {taskId} = request?.params;
+
+    const status = request.body;
+
+    if(!taskId){
+        throw new apiError(404, "Task id not found!")
+    }
+
+    const foundTask = await Task.findById(taskId);
+
+    if(!foundTask){
+        return response.status(404)
+        .json(
+            new apiResponse(404, {}, "Task not found, task may be deleted")
+        )
+    }
+
+    const updatedTask = await Task.findByIdAndUpdate(taskId, {
+        $set: {
+            taskStatus: status.status
+        }
+    }, {new: true}).select("-__v -_id");
+
+    if(!updatedTask){
+        throw new apiError(500, "Something went wrong while changing the status of the task")
+    }
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, updatedTask, "Task status updated successfully")
+    )
+
+});
+
+export {changeTaskStatus}
