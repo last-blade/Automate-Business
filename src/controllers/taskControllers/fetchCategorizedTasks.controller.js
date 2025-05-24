745ed9c43611353929a6360cc461792047382c1f
last-blade <meprashanttyagi2000@gmail.com>
2025-05-24 12:41:31 +0530

    Add fetchCategorizedTasks controller for retrieving tasks by status using aggregation pipelines

---
+++ src/controllers/taskControllers/fetchCategorizedTasks.controller.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+import { apiResponse, asyncHandler, Task } from "../allImports.js";
+
+const fetchCategorizedTasks = asyncHandler(async (request, response) => {
+    const {query} = request.query;
+
+    const userId = request.user?.id;
+
+    const categorizedTasks = await Task.aggregate([
+        {
+            $match: {
+                taskAssignedTo: new mongoose.Types.ObjectId(userId)
+            }
+        },
+
+        {
+            $group: {
+                _id: "$taskStatus",
+                totalTasks: {$sum: 1},
+                allTasks: {$push: "$$ROOT"}
+            }
+        },
+
+        {
+            $match: {
+                _id: query
+            }
+        },
+
+        {
+            $unwind: "$allTasks"
+        }
+    ]);
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, categorizedTasks, "Fecthed")
+    )
+
+});
+
+export {fetchCategorizedTasks}
