ce8fec273d1e58885daf64e261067b210ae72b37
last-blade <meprashanttyagi2000@gmail.com>
2025-05-26 12:23:51 +0530

    Add addNewTeamMember controller for managing new team member additions

---
+++ src/controllers/userControllers/addNewTeamMember.controller.js
@@ -0,0 +1,49 @@
+import { apiError, apiResponse, asyncHandler, NewMember, User } from "../allImports.js";
+
+const addNewTeamMember = asyncHandler(async (request, response) => {
+    const {fullname, email, whatsappNumber, accountType, reportingManager, password} = request.body;
+
+    if([fullname, email, whatsappNumber, accountType, reportingManager, password].some((inputField) => inputField === "")){
+        throw new apiError(404, "All fields are required")
+    }
+
+    const foundUser = await User.findOne({email: email});
+
+    if(foundUser){
+        throw new apiError(400, "User with this email or whatsapp number already exists")
+    }
+
+    const createdUser = await User.create({
+        fullname, 
+        email, 
+        whatsappNumber, 
+        accountType, 
+        reportingManager, 
+        password,
+    });
+
+    const isUserCreated = await User.findById(createdUser._id).select("-password -__v");
+
+    if(!isUserCreated){
+        throw new apiError(500, "Something went wrong, while adding a new member")
+    }
+
+    const newTeamMember = await NewMember.create({
+        newMemberCreatedBy: request.user?.id,
+        newMember: isUserCreated._id,
+    });
+
+    const foundNewMember = await NewMember.findById(newTeamMember._id);
+
+    if(!foundNewMember){
+        throw new apiError(500, "Something went wrong, while adding a new member")
+    }
+
+    return response.status(201)
+    .json(
+        new apiResponse(201, isUserCreated, "New team member added successfully")
+    )
+
+});
+
+export {addNewTeamMember}
