31a9bbbb822d0a4ee4d0e8b3e18e83143252d871
last-blade <meprashanttyagi2000@gmail.com>
2025-05-26 17:41:46 +0530

    Reorganize task routes for improved readability and maintainability and added edit-task route

--- src/routes/task.routes.js
+++ src/routes/task.routes.js
@@ -7,19 +7,23 @@ import { deleteTask } from "../controllers/taskControllers/deleteTask.controller
 import { changeTaskStatus } from "../controllers/taskControllers/changeTaskStatus.controller.js";
 import { fetchCategorizedTasks } from "../controllers/taskControllers/fetchCategorizedTasks.controller.js";
 import { fetchCategorizedTasksCounting } from "../controllers/taskControllers/fetchCategorizedTasksCounting.controller.js";
+import { editTask } from "../controllers/taskControllers/editTask.controller.js";
 
 const router = Router();
 
+//POST
 router.route("/create-task").post(authMiddleware, createTask);
-
-router.route("/view-task/:taskId").get(authMiddleware, viewTask);
-router.route("/fetch-all-tasks").get(authMiddleware, fetchUsersAllTasks);
-
-router.route("/delete-task/:taskId").delete(authMiddleware, deleteTask);
-
 router.route("/change-task-status/:taskId").post(authMiddleware, changeTaskStatus);
 
-router.route("/fetch-tasks").get(authMiddleware, fetchCategorizedTasks);
+//DELETE
+router.route("/delete-task/:taskId").delete(authMiddleware, deleteTask);
 
+//GET
+router.route("/view-task/:taskId").get(authMiddleware, viewTask);
+router.route("/fetch-all-tasks").get(authMiddleware, fetchUsersAllTasks);
+router.route("/fetch-tasks").get(authMiddleware, fetchCategorizedTasks);
 router.route("/categorywise-task-counting").get(authMiddleware, fetchCategorizedTasksCounting);
+
+//PUT
+router.route("/edit-task/:taskId").put(authMiddleware, editTask);
 export default router;
