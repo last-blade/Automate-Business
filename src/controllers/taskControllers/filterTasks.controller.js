85bad70e393a9e1f4422a3b6519f800a54a1c8ef
last-blade <meprashanttyagi2000@gmail.com>
2025-05-27 15:23:04 +0530

    Add filterTasks controller to retrieve tasks based on category, priority, and status

---
+++ src/controllers/taskControllers/filterTasks.controller.js
@@ -0,0 +1,46 @@
+import mongoose from "mongoose";
+import { apiError, apiResponse, asyncHandler, Task } from "../allImports.js";
+
+const filterTasks = asyncHandler(async (request, response) => {
+    const { taskCategory, taskPriority, taskStatus } = request.body;
+
+    if (!taskCategory && !taskPriority && !taskStatus) {
+        throw new apiError(400, "Select at least one field to filter");
+    }
+
+    const filter = {
+        taskCreatedBy: new mongoose.Types.ObjectId(request.user?.id)
+    };
+
+//---------------------------------------------------------------------------------
+    //!this below one also working fine
+    // const foltereTaks = await Task.find({
+    //     taskCreatedBy: request.user?.id,
+    //     taskCategory: "Reporting",
+    //     taskPriority: "High",
+    //     taskStatus: "Pending" 
+    // });
+//--------------------------------------------------------------------------------
+
+    if (taskCategory) {
+        filter.taskCategory = taskCategory
+    };
+
+    if (taskPriority) {
+        filter.taskPriority = taskPriority
+    };
+
+    if (taskStatus) {
+        filter.taskStatus = taskStatus
+    };
+
+    const filteredTasks = await Task.find(filter);
+
+    console.log(filter)
+
+    return response.status(200).json(
+        new apiResponse(200, {filteredTasks, totalTasks: filteredTasks.length}, "Filtered tasks fetched successfully")
+    );
+});
+
+export { filterTasks };
