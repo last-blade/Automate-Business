2ac4af11b63dccabbeb20ab817aba0b39ca02f1d
last-blade <meprashanttyagi2000@gmail.com>
2025-05-28 10:29:25 +0530

    Add sendOTP controller to handle OTP email verification

---
+++ src/controllers/userControllers/sendOTP.controller.js
@@ -0,0 +1,24 @@
+import { apiError, apiResponse, asyncHandler } from "../allImports.js";
+import { sendOtpToEmail } from "../../utils/sendOtpToEmail.js";
+
+const sendOTP = asyncHandler(async (request, response) => {
+    const { email } = request.body;
+
+    if(!email){
+        throw new apiError(404, "Email is required for OTP")
+    }
+
+    await sendOtpToEmail(email, "Your OTP for verification on KPS Automate Business Solutions");
+
+    const options = {
+        httpOnly: true,
+        secure: true,
+        maxAge: 2 * 60 * 1000,
+    };
+
+    return response.status(200)
+        .cookie("Email", email, options)
+        .json(new apiResponse(200, {}, "OTP sent successfully."));
+});
+
+export {sendOTP}
