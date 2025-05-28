4b1e42e0683183d63bf0a60f948ff499cbeb684b
last-blade <meprashanttyagi2000@gmail.com>
2025-05-28 12:20:49 +0530

    Add fetchMyTasksAssignedByLeader controller to retrieve tasks assigned to the user using Pipeline

---
+++ src/controllers/taskControllers/fetchMyTasksAssignedByLeader.controller.js
@@ -0,0 +1,52 @@
+import mongoose from "mongoose";
+import { apiResponse, asyncHandler, Task } from "../allImports.js";
+
+const fetchMyTasksAssignedByLeader = asyncHandler(async (request, response) => {
+
+    const userId = request.user?.id;
+
+    const page = parseInt(request.query.page) || 1;
+    const limit = parseInt(request.query.limit) || 10;
+    const skip = (page - 1) * 10;
+
+    const myTasksAssignedByLeader = await Task.aggregate([
+        {
+            $match: {
+                taskAssignedTo: new mongoose.Types.ObjectId(userId)
+            }
+        },
+
+        {
+            $skip: skip
+        },
+
+        {
+            $limit: limit
+        },
+
+        {
+            $sort: {
+                createdAt: -1
+            }
+        },
+
+        {
+            $project: {_id: 0, __v: 0, taskAssignedTo: 0}
+        }
+    ]);
+
+    if(myTasksAssignedByLeader.length === 0){
+        return response.status(200)
+        .json(
+            new apiResponse(200, myTasksAssignedByLeader, "No task assigned to you")
+        )
+    }
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, myTasksAssignedByLeader, "Task assigned to you fetched successfully")
+    )
+
+});
+
+export {fetchMyTasksAssignedByLeader};
