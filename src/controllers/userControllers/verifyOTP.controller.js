05cfd76b04ff4f3f3c5567d60fd21c49fb63c03a
last-blade <meprashanttyagi2000@gmail.com>
2025-05-28 10:29:48 +0530

    Add verifyOTP controller to handle OTP verification logic

---
+++ src/controllers/userControllers/verifyOTP.controller.js
@@ -0,0 +1,23 @@
+import { verifyOtpHelper } from "../../utils/verifyOtpHelper.js";
+import { apiError, apiResponse, asyncHandler } from "../allImports.js";
+
+const verifyOTP = asyncHandler(async (request, response) => {
+    const {incomingOTP} = request.body;
+    const email = request.cookies?.Email;
+
+    if(!incomingOTP){
+        throw new apiError(400, "OTP is required.")
+    }
+    
+    const isOtpValid = await verifyOtpHelper(email, incomingOTP);
+
+    if(isOtpValid){
+        return response.status(200)
+        .json(
+            new apiResponse(200, {}, "OTP Verified.")
+        )
+    }
+    
+});
+
+export {verifyOTP}
