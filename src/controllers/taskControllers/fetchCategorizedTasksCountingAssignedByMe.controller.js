ddda0c5ed5471b9cb1557ba89cc40a08cc31a60f
last-blade <meprashanttyagi2000@gmail.com>
2025-05-30 14:52:47 +0530

    Add controller to fetch categorized tasks counting assigned by the user

---
+++ src/controllers/taskControllers/fetchCategorizedTasksCountingAssignedByMe.controller.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+import { apiResponse, asyncHandler, Task } from "../allImports.js";
+
+const fetchCategorizedTasksCountingAssignedByMe = asyncHandler(async (request, response) => {
+    const userId = request.user?.id;
+
+    const allTasksCounting = await Task.aggregate([
+        {
+            $match: {
+                taskCreatedBy: new mongoose.Types.ObjectId(userId)
+            }
+        },
+
+        {
+            $group: {
+                _id: "$taskStatus",
+                totalTasks: {
+                    $sum: 1
+                }
+            }
+        },
+
+        {
+            $sort: {
+                totalTasks: 1
+            }
+        }
+    ]);
+
+    if(allTasksCounting.length === 0){
+        return response.status(200)
+        .json(
+            new apiResponse(200, {}, "No task found")
+        )
+    }
+
+    return response.status(200)
+    .json(
+        new apiResponse(200, allTasksCounting, "Total categorised tasks counting fetched")
+    )
+
+});
+
+export {fetchCategorizedTasksCountingAssignedByMe}
