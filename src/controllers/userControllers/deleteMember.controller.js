6b688ea78742617e0680ed2c1ac9694219f3dbfd
last-blade <meprashanttyagi2000@gmail.com>
2025-05-30 12:31:02 +0530

    Send email notification upon successful team member deletion

--- src/controllers/userControllers/deleteMember.controller.js
+++ src/controllers/userControllers/deleteMember.controller.js
@@ -1,3 +1,4 @@
+import accountDeletedEmail from "../../emails/userEmails/accountDeletedEmail.js";
 import { apiError, apiResponse, asyncHandler, NewMember, Task, User } from "../allImports.js";
 
 const deleteMember = asyncHandler(async (request, response) => {
@@ -10,7 +11,7 @@ const deleteMember = asyncHandler(async (request, response) => {
     const foundMember = await NewMember.findOne({
         newMemberCreatedBy: request.user?.id,
         newMember: memberId,
-    });
+    }).populate("newMember", "fullname email");
 
     if(!foundMember){
         throw new apiError(404, "Member not found, maybe deleted")
@@ -27,9 +28,14 @@ const deleteMember = asyncHandler(async (request, response) => {
         taskAssignedTo: memberId,
     });
 
+    await accountDeletedEmail({
+        fullname: foundMember.newMember.fullname,
+        email: foundMember.newMember.email,
+    });
+
     return response.status(200)
     .json(
-        new apiResponse(200, {}, "Team member deleted successfully")
+        new apiResponse(200, {}, `Team member '${foundMember.newMember.fullname}' deleted successfully`)
     )
 
 });
