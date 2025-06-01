754ca52111ef8abc242588b0d8bbfbe36c269d1e
last-blade <meprashanttyagi2000@gmail.com>
2025-06-01 10:36:56 +0530

    Add required validation to comment model fields

--- src/models/comment.model.js
+++ src/models/comment.model.js
@@ -3,21 +3,25 @@ import mongoose, { Schema } from "mongoose";
 const commentSchema = new Schema({
     comment: {
         type: String,
+        required: true,
     },
 
     commentedTask: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "Task"
+        ref: "Task",
+        required: true,
     },
 
     commentedBy: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User",
+        required: true,
     },
 
     thisTaskAssignedTo: {
         type: mongoose.Schema.Types.ObjectId,
         ref: "User", 
+        required: true,
     }
 
 }, {timestamps: true});
