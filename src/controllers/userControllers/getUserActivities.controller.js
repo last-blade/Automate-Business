a9a691a47b84f6167fd20b29a25236ccf483d2dc
last-blade <meprashanttyagi2000@gmail.com>
2025-06-02 14:53:31 +0530

    Fix unwind operation in getUserActivities to preserve null taskInfo

--- src/controllers/userControllers/getUserActivities.controller.js
+++ src/controllers/userControllers/getUserActivities.controller.js
@@ -37,9 +37,11 @@ const getUserActivities = asyncHandler(async (request, response) => {
         },
 
         {
-            $unwind: "$taskInfo"
+            $unwind: {
+                path: "$taskInfo",
+                preserveNullAndEmptyArrays: true
+            }
         },
-
 
         {
             $project: {
