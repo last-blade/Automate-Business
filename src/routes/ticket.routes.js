2e4e6d0c81c04d4bb6fd762af3d774c49fab8983
last-blade <meprashanttyagi2000@gmail.com>
2025-06-07 11:38:57 +0530

    Add changeTicketStatus route for updating ticket status

--- src/routes/ticket.routes.js
+++ src/routes/ticket.routes.js
@@ -2,10 +2,15 @@ import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware.js";
 import { raiseTicket } from "../controllers/supportControllers/raiseTicket.controller.js";
 import { handleMulterUpload } from "../middlewares/handleMulterUpload.middleware.js";
+import { authorizeRoles } from "../middlewares/autorizeRoles.middleware.js";
+import { changeTicketStatus } from "../controllers/supportControllers/changeTicketStatus.controller.js";
 
 const router = Router();
 
 //POST
 router.route("/rasie-ticket").post(authMiddleware, handleMulterUpload("attachFile"), raiseTicket);
+
+//PATCH
+router.route("/change-ticket-status/:ticketId").patch(authMiddleware, authorizeRoles("Support"), changeTicketStatus);
 
 export default router;
