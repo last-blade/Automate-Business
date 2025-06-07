142c0b39bb1db52ca5e4f27a058e3cb5c8900c0a
last-blade <meprashanttyagi2000@gmail.com>
2025-06-07 11:37:31 +0530

    Refactor authorizeRoles middleware to improve role checking and error response format

--- src/middlewares/autorizeRoles.middleware.js
+++ src/middlewares/autorizeRoles.middleware.js
@@ -3,16 +3,21 @@ import { asyncHandler } from "../utils/asyncHandler.js";
 
 const authorizeRoles = (...allowedRoles) => {
   return asyncHandler(async (request, response, next) => {
-    if (!request.user || !allowedRoles.includes(request.user.role)) {
-      return response.status(403)
-        .json(
-            new apiError(403, "Access denied")
-        );
+    const userRole = request.user?.accountType;
+
+    if (!request.user || !allowedRoles.includes(userRole)) {
+      console.log("Access Denied: user role is", userRole);
+      return response.status(403).json({
+          "statusCode": 403,
+          "success": false,
+          "data": null,
+          "errors": ["Access denied"]
+        });
     }
 
     next();
-    
   });
 };
+
 
 export { authorizeRoles };
