0cf788d78b9cedf546f94edc95cf9d58ba3e6163
last-blade <meprashanttyagi2000@gmail.com>
2025-07-28 14:46:55 +0530

    add cronjob

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express, { urlencoded } from "express"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser";
 import cors from "cors"
+import "./cron/taskReminderCron.js";
 
 dotenv.config({
     path: "./.env"
