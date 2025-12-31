efb4f8a43a78ce4ef9901d9d3c5e3383f134519a
last-blade <meprashanttyagi2000@gmail.com>
2025-12-31 11:05:35 +0530

    add meetingNote schema for storing meeting notes

---
+++ src/models/meetingNote.model.js
@@ -0,0 +1,48 @@
+import mongoose, { Schema } from "mongoose";
+
+const meetingNoteSchema = new Schema({
+    meetingTitle: {
+        type: String,
+        trim: true,
+        required: true,
+    },
+
+    meetingDate: {
+        type: Date,
+        required: true,
+    },
+
+    department: {
+        type: String,
+        default: null,
+    },
+
+    meetingMode: {
+        type: String,
+        enum: ["Online", "Offline"],
+        default: "Offline",
+    },
+
+    meetingMembers: {
+        type: String,
+        default: null,
+    },
+
+    meetingDescription: {
+        type: String,
+        required: true,
+    },
+
+    meetingNoteCreatedBy: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+    }
+
+}, {timestamps: true});
+
+meetingNoteSchema.index({meetingDate: 1});
+meetingNoteSchema.index({meetingMode: 1});
+meetingNoteSchema.index({meetingNoteCreatedBy: 1});
+
+export const MeetingNote = mongoose.model("MeetingNote", meetingNoteSchema);
